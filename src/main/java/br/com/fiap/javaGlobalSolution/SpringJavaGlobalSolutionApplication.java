51bc4137fafa24e1d00accf0bb6b62d05276cbfa
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-29 10:22:25 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/javaGlobalSolution/SpringJavaGlobalSolutionApplication.java
@@ -0,0 +1,14 @@
+
+package br.com.fiap.concessionaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPjCp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run( SpringPjCp2Application.class, args );
+    }
+
+}
---
+++ src/test/java/br/com/fiap/javaGlobalSolution/SpringJavaGlobalSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.concessionaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPjCp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
